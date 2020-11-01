bee8a70e4272d6b8bef083b694e8abc709274906
LuK1337 <priv.luk@gmail.com>
2020-11-01 13:26:48 -0500

    Settings: Add GZOSP entries into device info
    Author: Harry Youd <harry@harryyoud.co.uk>
    Date:   Fri Nov 24 09:49:51 2017 -0800
    
        Settings: Add GZOSP version, API level and build date to device info
    
        Change-Id: I4acf834442712954f8e9db15370bffafd4547174
    
    Author: Tobias Tefke <tobias.tefke@tutanota.com>
    Date:   Wed Jun 13 11:27:22 2018 +0200
    
        [1/2] Add vendor security patch level to device info
    
        Change-Id: I523fcce65cc4155591c12ea1ac04807ebfc5c716
    
    Change-Id: I8d4c6869f8dfa34a96567bec18f9f8276b66a64e

--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -35,7 +35,6 @@ import com.android.settings.deviceinfo.FccEquipmentIdPreferenceController;
 import com.android.settings.deviceinfo.FeedbackPreferenceController;
 import com.android.settings.deviceinfo.IpAddressPreferenceController;
 import com.android.settings.deviceinfo.ManualPreferenceController;
-import com.android.settings.deviceinfo.ROMVersionPreferenceController;
 import com.android.settings.deviceinfo.UptimePreferenceController;
 import com.android.settings.deviceinfo.WifiMacAddressPreferenceController;
 import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
@@ -108,7 +107,6 @@ public class MyDeviceInfoFragment extends DashboardFragment
         controllers.add(new FeedbackPreferenceController(fragment, context));
         controllers.add(new FccEquipmentIdPreferenceController(context));
         controllers.add(new UptimePreferenceController(context, lifecycle));
-        controllers.add(new ROMVersionPreferenceController(context));
         return controllers;
     }
 
